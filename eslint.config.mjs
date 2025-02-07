e81d3238f2013e630a5f12563e945b5632e09836
sahil-raj <sahilrajaaru@gmail.com>
2025-02-07 05:54:12 +0530

    chore: linting off

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,28 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
 
-export default eslintConfig;
+// export default eslintConfig;
+
+export default {
+  root: true,
+  extends: [],
+  rules: {
+    // Turn off all rules by default
+    "no-unused-vars": "off",
+    "no-console": "off",
+    "@typescript-eslint/no-empty-object-type": "off", // Disable this specific rule as well
+    // Add any other rules you want to disable here
+  },
+};
